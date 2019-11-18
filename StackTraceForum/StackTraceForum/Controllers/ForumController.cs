90f56ba19f26f073f92028f893fe73f53b01a1b5
PeterFred <32250775+PeterFred@users.noreply.github.com>
2019-11-19 11:27:45 +1300

    Forum Controller created

---
+++ StackTraceForum/StackTraceForum/Controllers/ForumController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackTraceForums.Data;
+
+namespace StackTraceForum.Controllers
+{
+    public class ForumController : Controller
+    {
+        private readonly IForum _forumService;
+
+        public ForumController(IForum forumService)
+        {
+            _forumService = forumService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
