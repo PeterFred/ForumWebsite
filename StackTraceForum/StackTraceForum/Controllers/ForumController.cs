c95e98cd99729159101c580413bade78ca701c1a
PeterFred <32250775+PeterFred@users.noreply.github.com>
2019-11-25 07:57:41 +1300

    Updated Index controller, Added ForumListingModel

--- StackTraceForum/StackTraceForum/Controllers/ForumController.cs
+++ StackTraceForum/StackTraceForum/Controllers/ForumController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using StackTraceForums.Data;
+using System.Collections.Generic;
 
 namespace StackTraceForum.Controllers
 {
@@ -12,8 +13,18 @@ namespace StackTraceForum.Controllers
             _forumService = forumService;
         }
 
+
+        //Forum object is EntityModel - maps c# object back to 
+        //the represtatio of this data in the data store.
+        //Don't pass the raw entity model to the view, pass a viewModel instead.
+        //Index page doen't need a full form object, create a simplified view model to pass down
+
         public IActionResult Index()
         {
+            IEnumerable<Forum> forums = _forumService.GetAll();
+
+
+
             return View();
         }
     }
