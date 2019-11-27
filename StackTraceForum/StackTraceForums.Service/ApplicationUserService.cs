16835f4026c42bc318c057e269b9894737df3d02
PeterFred <32250775+PeterFred@users.noreply.github.com>
2019-11-27 20:51:28 +1300

    Create ProfileController, AppUser, UploadServices

---
+++ StackTraceForum/StackTraceForums.Service/ApplicationUserService.cs
@@ -0,0 +1,61 @@
+﻿using Forum.Data;
+using Forum.Data.Interfaces;
+using Forum.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forum.Service
+{
+    class ApplicationUserService : IApplicationUser
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ApplicationUserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(ApplicationUser user)
+        {
+            _context.Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public ApplicationUser GetByName(string name)
+        {
+            return _context.ApplicationUsers
+                .FirstOrDefault(user => user.UserName == name);
+        }
+
+        public IEnumerable<ApplicationUser> GetAll()
+        {
+            return _context.ApplicationUsers;
+        }
+
+        public ApplicationUser GetById(string id)
+        {
+            return GetAll()
+                .FirstOrDefault(user => user.Id == id);
+        }
+
+        public async Task IncrementRating(string id)
+        {
+            var user = GetById(id);
+            user.Rating += 1;
+            _context.Update(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task SetProfileImage(string id, Uri uri)
+        {
+            var user = GetById(id);
+            user.ProfileImageUrl = uri.AbsoluteUri;
+            _context.Update(user);
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
