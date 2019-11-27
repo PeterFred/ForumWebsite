ed908e7b6413260b998272aad37428a63053dc54
PeterFred <32250775+PeterFred@users.noreply.github.com>
2019-11-27 20:51:45 +1300

    Add to commit ProfileController

---
+++ StackTraceForum/StackTraceForum/Controllers/ProfileController.cs
@@ -0,0 +1,30 @@
+﻿using Forum.Data.Interfaces;
+using Forum.Data.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forum.Web.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IApplicationUser _userService;
+        private readonly IUpload _uploadService;
+
+        public ProfileController(UserManager<ApplicationUser> userManager, IApplicationUser userService, IUpload uploadService)
+        {
+            _userManager = userManager;
+            _userService = userService;
+            _uploadService = uploadService;
+        }
+
+        [Authorize]
+        public IActionResult Detail(string id)
+        {
+           
+
+            return View(model);
+        }
+    }
+}
